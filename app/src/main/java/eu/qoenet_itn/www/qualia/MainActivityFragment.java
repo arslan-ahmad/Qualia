ac877dd91725bf4adfd4aeca000715870af40fee
Arslan <arslan.ahmad.sh@gmail.com>
2016-09-01 18:29:55 +0100

    Detail activity fragement added

--- app/src/main/java/eu/qoenet_itn/www/qualia/MainActivityFragment.java
+++ app/src/main/java/eu/qoenet_itn/www/qualia/MainActivityFragment.java
@@ -1,10 +1,12 @@
 package eu.qoenet_itn.www.qualia;
 
-import android.support.v4.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +19,23 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+            View rootView=inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onActivityCreated(savedInstanceState);
+        // get the button view
+        ImageView StartButton = (ImageView) getView().findViewById(R.id.startbutton);
+        // set a onclick listener for when the button gets clicked
+        StartButton.setOnClickListener(new View.OnClickListener() {
+            // Start new list activity
+            public void onClick(View v) {
+                Intent mainIntent = new Intent(getActivity(),
+                        DetailActivity.class);
+                startActivity(mainIntent);
+            }
+        });
     }
 }
